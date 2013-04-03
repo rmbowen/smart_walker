e1374ad9147b44116f5840e749fa7af5358dcd9e
Thomas DeMeo <thomasdemeo@gmail.com>
2013-04-02 22:25:04 -0400

    Added Basic CommandParser Class and parsing of entered command string;

---
+++ SmartWalkerApplication/Commands/CommandParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWalkerApplication.Commands
+{
+    class CommandParser
+    {
+
+        public CommandParser()
+        {
+        }
+
+        public void start()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a command: ");
+                string input = Console.ReadLine();
+                Console.WriteLine();
+                string[] words = input.Split(' ');
+                bool invalidCommand = true;
+                if (words[0].Equals("Mic"))
+                {
+                    if (words.Length == 1)
+                    {
+                        Console.WriteLine("Mic Array Command Entered");
+                        invalidCommand = false;
+                    }
+                }
+                else if(words[0].Equals("Navigation"))
+                {
+                    if (words.Length == 2)
+                    {
+                        Console.WriteLine("Navigation Command Entered");
+                        invalidCommand = false;
+                    }
+                }
+                else if (words[0].Equals("Force"))
+                {
+                    if (words.Length == 2)
+                    {
+                        Console.WriteLine("Force Command Entered");
+                        invalidCommand = false;
+                    }
+                }
+                if (invalidCommand) {
+                    Console.WriteLine(input + " is not a valid command");
+                    Console.WriteLine("Valid Commands Include:");
+                    Console.WriteLine("Mic (read microphone array)");
+                    Console.WriteLine("Navigation [degree] (start navigation system)");
+                    Console.WriteLine("Force [Left/Right] (read force sensors)");
+                    Console.WriteLine();
+
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ SmartWalkerApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SmartWalkerApplication.Commands;
+
+namespace SmartWalkerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandParser parser = new CommandParser();
+            parser.start();
+        }
+    }
+}
